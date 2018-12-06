61ca681d0e290f65124c18f67311db12b4fa6f3d
JiCanyu <15626174199@163.com>
2018-12-06 17:26:58 +0800

    1 test

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Ryan on 2018/12/6
+ */
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    @RequestMapping("say")
+    public String sayHello() {
+        return  "aaaaaaaaaaa";
+    }
+}
---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+/**
+ * @author Ryan
+ * @date 2018/12/6
+ */
+public class Test {
+}
